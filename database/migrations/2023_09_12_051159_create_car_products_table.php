f06b75f4bae868dccba540e98563a5d582e95644
Subrata8778 <93910718+Subrata8778@users.noreply.github.com>
2023-09-12 12:30:54 +0700

    Migration Product

--- database/migrations/2023_09_12_051159_create_car_products_table.php
+++ database/migrations/2023_09_12_051159_create_car_products_table.php
@@ -15,7 +15,23 @@ class CreateCarProductsTable extends Migration
     {
         Schema::create('car_products', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->text("photo");
+            $table->year("year");
+            $table->double("price");
+            $table->double("kilometre");
+            $table->enum("tranmission", ["Manual, Automatic, Hybrid"]);
+            $table->integer("capacity");
+            $table->enum("fuel", ["Petrol, Diesel, Electricity"]);
+            $table->longtext("description");
+            $table->enum("car_status", ["Available, Sold"])->default("Available");
+            $table->enum("status", ["Processed, Approved, Finished, Cancelled"]);
+            $table->unsignedBigInteger("car_brand_id");
+            $table->unsignedBigInteger("car_category_id");
             $table->timestamps();
+
+            $table->foreign("car_brand_id")->references("id")->on("car_brands");
+            $table->foreign("car_category_id")->references("id")->on("car_categories");
         });
     }
 

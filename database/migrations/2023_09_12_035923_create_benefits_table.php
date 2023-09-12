0faf33b9192cea0c4792fab32bf4e3044d7ae15a
Subrata8778 <93910718+Subrata8778@users.noreply.github.com>
2023-09-12 11:37:38 +0700

    Migrations
    Activities, companies, missions, benefits, users

--- database/migrations/2023_09_12_035923_create_benefits_table.php
+++ database/migrations/2023_09_12_035923_create_benefits_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBenefitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('benefits', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('telephone')->unique();
-            $table->rememberToken();
+            $table->text("benefit");
+            $table->unsignedBigInteger("companies_id");
             $table->timestamps();
+
+            $table->foreign("companies_id")->references("id")->on("companies");
         });
     }
 
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('benefits');
     }
 }
